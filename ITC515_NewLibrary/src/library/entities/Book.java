e50c06bda5e88c1a0467ea5d1f905a7809278c60
amayer06 <katbat_@hotmail.com>
2015-09-20 10:00:39 +1000

    Book Entity and TestClass have been created.

---
+++ ITC515_NewLibrary/src/library/entities/Book.java
@@ -0,0 +1,29 @@
+package library.entities;
+
+public class Book {
+
+	private int id;
+	private String author;
+	private String title;
+	private String callNumber;
+	
+	public int getID()
+	{
+		return id;
+	}
+	
+	public String getAuthor()
+	{
+		return author;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public String getCallNumber()
+	{
+		return callNumber;
+	}
+}
---
+++ ITC515_NewLibrary/src/test/lowlevelunittesting/BookTest.java
@@ -0,0 +1,13 @@
+package test.lowlevelunittesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookTest {
+	
+	@Test
+	public void testBorrow() {
+	
+	}
+}
