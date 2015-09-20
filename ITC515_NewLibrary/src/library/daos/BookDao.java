897b1b8401162b90f1a8c8befdda0ee2a6f5f6ac
amayer06 <katbat_@hotmail.com>
2015-09-20 11:03:35 +1000

    Started to implement BookDao

--- ITC515_NewLibrary/src/library/daos/BookDao.java
+++ ITC515_NewLibrary/src/library/daos/BookDao.java
@@ -7,11 +7,13 @@ import library.entities.Book;
 public interface BookDao {
 
 	public List<Book> getId();
-	public List<Book> getAuthor();
-	public List<Book> getTitle();
-	public List<Book> getCallNumber();
+//	public List<Book> getAuthor();
+//	public List<Book> getTitle();
+//	public List<Book> getCallNumber();
 	
-	public Book getId(String id);
-	public void ILoan(Book loan_);
-	public void EBookState(Book state_);
+	public Book getAuthor(String author);
+	public Book getTitle(String title);
+	public Book getCallNumber(String callNumber);
+	public void ILoan(Book loan);
+	public void EBookState(Book state);
 }
