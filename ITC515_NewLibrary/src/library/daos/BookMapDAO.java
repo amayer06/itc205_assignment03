f443e3aa15cd70c4af4e4463b99c2839dfc13aca
amayer06 <katbat_@hotmail.com>
2015-10-02 21:05:54 +1000

    Fixed BookMapDAO and BookMapDAOTest.

--- ITC515_NewLibrary/src/library/daos/BookMapDAO.java
+++ ITC515_NewLibrary/src/library/daos/BookMapDAO.java
@@ -11,17 +11,18 @@ import library.interfaces.daos.IBookDAO;
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
-public class BookMapDAO implements IBookDAO{
+public class BookMapDAO implements IBookDAO {
 
 	private int nextId_;
 	private Map<Integer, IBook> bookMap_;
 	private IBookHelper helper_;
-	
+
 	public BookMapDAO (IBookHelper helper)
 	{
 		if(helper == null)
+		{
 			throw new IllegalArgumentException ("Error: Helper is null.");
-	
+		}
 		this.helper_ = helper;
 		nextId_ = 1;
 		bookMap_ = new HashMap<Integer, IBook>();
@@ -31,32 +32,37 @@ public class BookMapDAO implements IBookDAO{
 	
 	@Override
 	public IBook addBook(String author, String title, String callNo) {
-		
-		if(author == null || author.isEmpty())
-			throw new IllegalArgumentException ("Error: Author is invalid.");
-		
-		if(title == null || title.isEmpty())
-			throw new IllegalArgumentException ("Error: Title is invalid.");
-		
-		if(callNo == null || callNo.isEmpty())
-			throw new IllegalArgumentException ("Error: Call Number is invalid.");
-		
+
+		if (author == null || author.isEmpty()) {
+			throw new IllegalArgumentException("Error: Author is invalid.");
+		}
+
+		if (title == null || title.isEmpty()) {
+			throw new IllegalArgumentException("Error: Title is invalid.");
+		}
+
+		if (callNo == null || callNo.isEmpty()) {
+			throw new IllegalArgumentException("Error: Call Number is invalid.");
+		}
+
 		IBook book = helper_.makeBook(author, title, callNo, nextId_);
-	
+
 		bookMap_.put(nextId_, book);
-		
+
 		nextId_++;
-		
+
 		return book;
 	}
 
 	
-
+	
 	@Override
 	public IBook getBookByID(int id) {
-		
-		if(bookMap_.containsKey(id))
+
+		if (bookMap_.containsKey(id))
+		{
 			return bookMap_.get(id);
+		}
 		else
 			return null;
 	}
@@ -65,9 +71,9 @@ public class BookMapDAO implements IBookDAO{
 	
 	@Override
 	public List<IBook> listBooks() {
-		
+
 		List<IBook> books = new ArrayList<IBook>(bookMap_.values());
-		
+
 		return books;
 	}
 
@@ -75,16 +81,15 @@ public class BookMapDAO implements IBookDAO{
 	
 	@Override
 	public List<IBook> findBooksByAuthor(String author) {
-	
+
 		List<IBook> authorList = new ArrayList<IBook>();
 		List<IBook> allBooks = new ArrayList<IBook>(bookMap_.values());
-	
-		for(int i=0; i<allBooks.size();i++)
-		{
-			if(allBooks.get(i).getAuthor().equals(author))
+
+		for (int i = 0; i < allBooks.size(); i++) {
+			if (allBooks.get(i).getAuthor().equals(author))
 				authorList.add(allBooks.get(i));
 		}
-		
+
 		return authorList;
 	}
 
@@ -92,18 +97,18 @@ public class BookMapDAO implements IBookDAO{
 	
 	@Override
 	public List<IBook> findBooksByTitle(String title) {
-		
+
 		List<IBook> bookList = new ArrayList<>();
 		List<IBook> allBooks = new ArrayList<IBook>(bookMap_.values());
-		
-		for(int i=0; i<allBooks.size();i++)
-		{
-			if(allBooks.get(i).getTitle().equals(title))
+
+		for (int i = 0; i < allBooks.size(); i++) {
+			if (allBooks.get(i).getTitle().equals(title))
 				bookList.add(allBooks.get(i));
 		}
-		
-		return bookList;			
+
+		return bookList;
 	}
+
 	
 	
 	@Override
@@ -111,13 +116,13 @@ public class BookMapDAO implements IBookDAO{
 
 		List<IBook> findBooks = new ArrayList<>();
 		List<IBook> allBooks = new ArrayList<IBook>(bookMap_.values());
-		
-		for(int i=0; i<allBooks.size();i++)
-		{
-			if(allBooks.get(i).getAuthor().equals(author) && allBooks.get(i).getTitle().equals(title))
+
+		for (int i = 0; i < allBooks.size(); i++) {
+			if (allBooks.get(i).getAuthor().equals(author)
+					&& allBooks.get(i).getTitle().equals(title))
 				findBooks.add(allBooks.get(i));
 		}
-		
+
 		return findBooks;
 	}
 }
--- ITC515_NewLibrary/src/library/testing/BookMapDAOTest.java
+++ ITC515_NewLibrary/src/library/testing/BookMapDAOTest.java
@@ -13,6 +13,7 @@ import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,107 +21,209 @@ import static org.mockito.Mockito.*;
 
 public class BookMapDAOTest {
 
-	IBookHelper mockBookHelper;
-	IBook mockBook1;	
+	IBookHelper mockBookHelper_;
+	IBook mockBook1;
 	IBook mockBook2;
-	BookMapDAO bookMapDao;
+	BookMapDAO bookMapDao_;
 	String name1 = "Simon Lowe";
 	String title1 = "The Bird";
 	String callNo1 = "JHJH";
-	
-	@Before
-    public void setUp()
-	{
-        mockBookHelper = mock(IBookHelper.class);
-        mockBook1 = mock(IBook.class);
-        mockBook2 = mock(IBook.class);
-        
-        when(mockBook1.getID()).thenReturn(1);
-        when(mockBook1.getAuthor()).thenReturn(name1);
-        when(mockBook1.getTitle()).thenReturn(title1);
-        
-        when(mockBook2.getID()).thenReturn(2);
-        when(mockBook2.getAuthor()).thenReturn("Sarah Bell");
-        when(mockBook2.getTitle()).thenReturn("The Castle");
-        
-        when(mockBookHelper.makeBook(name1, title1, callNo1, 1)).thenReturn(mockBook1);
-        when(mockBookHelper.makeBook("Sarah Bell", "The Castle", "IKIK", 2)).thenReturn(mockBook2);
-        
-        bookMapDao = new BookMapDAO(mockBookHelper);
+	String name2 = "Sarah Bell";
+	String title2 = "The Castle";
+	String callNo2 = "IKIK";
 
-    }
+	@Before
+	public void setUp() {
+		mockBookHelper_ = mock(IBookHelper.class);
+		mockBook1 = mock(IBook.class);
+		mockBook2 = mock(IBook.class);
+
+		when(mockBook1.getID()).thenReturn(1);
+		when(mockBook1.getAuthor()).thenReturn(name1);
+		when(mockBook1.getTitle()).thenReturn(title1);
+
+		when(mockBook2.getID()).thenReturn(2);
+		when(mockBook2.getAuthor()).thenReturn("Sarah Bell");
+		when(mockBook2.getTitle()).thenReturn("The Castle");
+
+		when(mockBookHelper_.makeBook(name1, title1, callNo1, 1)).thenReturn(
+				mockBook1);
+		when(mockBookHelper_.makeBook(name2, title2, callNo2, 2)).thenReturn(
+				mockBook2);
+
+		bookMapDao_ = new BookMapDAO(mockBookHelper_);
+	}
+
 	
 	
-	
-	public void clearSetUp()
-	{
-		mockBookHelper = null;
+	@After
+	public void clearSetUp() {
+		mockBookHelper_ = null;
 		mockBook1 = null;
 		mockBook2 = null;
-		
-		bookMapDao = null;
-	}
-	
-	
-	
-    @Test
-    public void testCreateBookMapDAO() 
-    {
-            bookMapDao = new BookMapDAO(mockBookHelper);
-           
-            assertNotNull(bookMapDao);
-           
-   }
 
-    @Test
-    public void testCreateBookMapDAOHelperNull() // ThrowsIllegalArgumentNull
-    {
-        try {
-            bookMapDao = new BookMapDAO(null);
-            fail("Should have thrown IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        }
-    }
+		bookMapDao_ = null;
+	}
+
+	
+	
+	@Test
+	public void testCreateBookMapDAO() {
+		
+		//execute
+		bookMapDao_ = new BookMapDAO(mockBookHelper_);
+
+		assertNotNull(bookMapDao_);
+
+	}
+
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateBookMapDAOHelperNull() {
+
+		//execute
+		bookMapDao_ = new BookMapDAO(null);
+
+		fail("Should have thrown IllegalArgumentException");
+	}
 
 	
 	
 	@Test
 	public void testAddBook() {
-	    
-	    //execute
-	    IBook actual = bookMapDao.addBook(name1, title1, callNo1);
-	    
-	    //asserts
-	    verify(mockBookHelper).makeBook(name1, title1, callNo1, 1);
-	    
-        assertEquals(actual, mockBook1);
- 	}
 
+		//execute
+		IBook actual = bookMapDao_.addBook(name1, title1, callNo1);
+
+		//asserts
+		verify(mockBookHelper_).makeBook(name1, title1, callNo1, 1);
+
+		assertEquals(actual, mockBook1);
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookAuthorIsNull() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook(null, title1, callNo1);
+
+		// asserts
+		verify(mockBookHelper_).makeBook(null, title1, callNo1, 1);
+
+		assertNull(actual.getAuthor());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookAuthorIsBlank() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook("", title1, callNo1);
+
+		// asserts
+		verify(mockBookHelper_).makeBook("", title1, callNo1, 1);
+
+		assertNull(actual.getAuthor());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookTitleIsNull() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook(name1, null, callNo1);
+
+		// asserts
+		verify(mockBookHelper_).makeBook(name1, null, callNo1, 1);
+
+		assertNull(actual.getTitle());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookTitleIsBlank() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook(name1, "", callNo1);
+
+		// asserts
+		verify(mockBookHelper_).makeBook(name1, "", callNo1, 1);
+
+		assertNull(actual.getTitle());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookCallNumberIsNull() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook(name1, title1, null);
+
+		// asserts
+		verify(mockBookHelper_).makeBook(name1, title1, null, 1);
+
+		assertNull(actual.getCallNumber());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+	
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddBookCallNumberIsBlank() {
+
+		/// execute
+		IBook actual = bookMapDao_.addBook(name1, title1, "");
+
+		// asserts
+		verify(mockBookHelper_).makeBook(name1, title1, "", 1);
+
+		assertNotNull(actual.getCallNumber());
+		
+		fail("Should have thrown IllegalArgumentException");
+	}
+	
 	
 	
 	@Test
 	public void testGetBookByID() {
-	    
-        //execute
-        IBook actual = bookMapDao.addBook(name1, title1, callNo1);
-        
-        IBook mapBook = bookMapDao.getBookByID(1);
-        
-        //assert
-        assertEquals(mapBook, mockBook1);
+
+		// execute
+		IBook actual = bookMapDao_.addBook(name1, title1, callNo1);
+
+		IBook mapBook = bookMapDao_.getBookByID(1);
+
+		// assert
+		assertEquals(mapBook, mockBook1);
 	}
 
 	
 	
 	@Test
 	public void testListBooks() {
-		
-		bookMapDao.addBook(name1, title1, callNo1);
-		bookMapDao.addBook("Sarah Bell", "The Castle", "IKIK");
-		
-		List<IBook> listBooks = bookMapDao.listBooks();
-		
+
+		//execute
+		bookMapDao_.addBook(name1, title1, callNo1);
+		bookMapDao_.addBook("Sarah Bell", "The Castle", "IKIK");
+
+		List<IBook> listBooks = bookMapDao_.listBooks();
+
+		// asserts
 		assertEquals(2, listBooks.size());
 		assertTrue(listBooks.contains(mockBook1));
 		assertTrue(listBooks.contains(mockBook2));
@@ -130,15 +233,16 @@ public class BookMapDAOTest {
 	
 	@Test
 	public void testFindBooksByAuthor() {
-		BookMapDAO bookDao = new BookMapDAO(mockBookHelper);
-		
-		bookDao.addBook("Simon Lowe", "The Bird", "JHJH");
-		bookDao.addBook("Sarah Bell", "The Castle", "IKIK");
-		
-		List<IBook> listBooks = bookDao.findBooksByAuthor("Simon Lowe");
-		
+
+		//execute
+		bookMapDao_.addBook("Simon Lowe", "The Bird", "JHJH");
+		bookMapDao_.addBook("Sarah Bell", "The Castle", "IKIK");
+
+		List<IBook> listBooks = bookMapDao_.findBooksByAuthor("Simon Lowe");
+
 		IBook theBird = listBooks.get(0);
-		
+
+		// asserts
 		assertEquals(1, listBooks.size());
 		assertEquals("Simon Lowe", theBird.getAuthor());
 	}
@@ -147,15 +251,16 @@ public class BookMapDAOTest {
 	
 	@Test
 	public void testFindBooksByTitle() {
-		BookMapDAO bookDao = new BookMapDAO(mockBookHelper);
 		
-		bookDao.addBook("Simon Lowe", "The Bird", "JHJH");
-		bookDao.addBook("Sarah Bell", "The Castle", "IKIK");
-		
-		List<IBook> listBooks = bookDao.findBooksByTitle("The Castle");
-		
+		//execute
+		bookMapDao_.addBook("Simon Lowe", "The Bird", "JHJH");
+		bookMapDao_.addBook("Sarah Bell", "The Castle", "IKIK");
+
+		List<IBook> listBooks = bookMapDao_.findBooksByTitle("The Castle");
+
 		IBook theCastle = listBooks.get(0);
-		
+
+		//asserts
 		assertEquals("The Castle", theCastle.getTitle());
 	}
 
@@ -163,18 +268,16 @@ public class BookMapDAOTest {
 	
 	@Test
 	public void testFindBooksByAuthorTitle() {
-		setUp();
-		
-		BookMapDAO bookDao = new BookMapDAO(mockBookHelper);
-		
-		bookDao.addBook("Simon Lowe", "The Bird", "JHJH");
-		bookDao.addBook("Sarah Bell", "The Castle", "IKIK");
-		
-		List<IBook> listBooks = bookDao.findBooksByAuthorTitle("Simon Lowe", "The Bird");
-				
+
+		//execute
+		bookMapDao_.addBook("Simon Lowe", "The Bird", "JHJH");
+		bookMapDao_.addBook("Sarah Bell", "The Castle", "IKIK");
+
+		List<IBook> listBooks = bookMapDao_.findBooksByAuthorTitle("Simon Lowe",
+				"The Bird");
+
+		//asserts
 		assertEquals(1, listBooks.size());
 		assertTrue(listBooks.contains(mockBook1));
-		
-		clearSetUp();
 	}
 }
